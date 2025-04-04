7d713a7924a36751a18d5fb3a551088df353f309
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-04-04 10:10:10 +0000

    soluzione

--- Esercizio.java
+++ Esercizio.java
@@ -2,25 +2,87 @@
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
+
 
 // Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+    public static void main(String[] args) {
+        int n, i;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        System.out.println("Inserire il numero di automobili");
+        n = Integer.parseInt(input.nextLine());
+        String[] nomi = new String[n];
+        int[] posizioni = new int[n];
+
+        // Caricamento dei due vettori
+        for (i = 0; i <= n - 1; i++) {
+            System.out.println("Nome " + (i + 1) + "° automobile");
+            nomi[i] = input.nextLine();
+        }
+        impostaVettoreRandom(posizioni);
+        visualizzaVettori(nomi, posizioni);
+
+        // Ordinamento dei vettori paralleli. Quando ordino Posizioni, ordino gli elementi corrispondenti di Nomi.
+        bubbleSort(nomi, posizioni);
+
+        // Visualizzo i valori ordinati.
+        System.out.println("Posizione di arrivo");
+        visualizzaVettori(nomi, posizioni);
+    }
+    
+    public static void bubbleSort(String[] nomi, int[] posizioni) {
+        
+        String ts;
+        int t;
+        boolean scambio;
+        int i, j;
+        int n = nomi.length;
+
+        scambio = true;
+        i = 0;
+        while (i <= n - 1 && scambio) {
+            scambio = false;
+            j = 0;
+            while (j <= n - 2 - i) {
+                if (posizioni[j] > posizioni[j + 1]) {
+                    scambio = true;
+
+                    // Prima ordino Posizioni
+                    t = posizioni[j];
+                    posizioni[j] = posizioni[j + 1];
+                    posizioni[j + 1] = t;
+
+                    // Per mantenere la corrispondenza fra nomi e posizioni, devo scambiare anche le celle dei nomi. In questo modo i vettori paralleli rimangono coerenti.
+                    ts = nomi[j];
+                    nomi[j] = nomi[j + 1];
+                    nomi[j + 1] = ts;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+    }
+    
+    public static void impostaVettoreRandom(int[] posizioni) {
+        int i;
+        int n = posizioni.length;
+
+        for (i = 0; i <= n - 1; i++) {
+            posizioni[i] = 1 + random.nextInt(n);
+        }
+    }
+    
+    public static void visualizzaVettori(String[] nomi, int[] posizioni) {
+        int i;
+        int n = posizioni.length;
+
+        for (i = 0; i <= n - 1; i++) {
+            System.out.println("Auto " + nomi[i] + "; Posizione: " + posizioni[i]);
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
